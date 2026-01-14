53def5f3d56e562739ac68ed8c4d5d1a5ffd5944
sol4nki <solankipranjal44@gmail.com>
2026-01-14 10:54:09 +0530

    partially imp
    dragNdrop partially implemented continue w/ it then work with ui improvements and figure out more options than the idk placeholder

--- webpage/script.js
+++ webpage/script.js
@@ -1,1 +1,39 @@
 // implement cookies for the name title and stuff
+
+
+
+
+// drag n drop code below tetetetette
+// source -> https://uploadcare.com/blog/how-to-make-a-drag-and-drop-file-uploader/
+
+const dropArea = document.getElementById('uploadpdf');
+const fileInput = document.getElementById('file-input');
+
+// Utility function to prevent default browser behavior
+function preventDefaults(e) {
+  e.preventDefault();
+  e.stopPropagation();
+}
+
+// Preventing default browser behavior when dragging a file over the container
+dropArea.addEventListener('dragover', preventDefaults);
+dropArea.addEventListener('dragenter', preventDefaults);
+dropArea.addEventListener('dragleave', preventDefaults);
+
+// Handling dropping files into the area
+dropArea.addEventListener('drop', handleDrop);
+
+// We’ll discuss `handleDrop` function down the road
+function handleDrop(e) { /* ... */ }
+
+// this is only for ui improvement
+dropArea.addEventListener('dragover', () => {
+  dropArea.classList.add('dragglow');
+});
+
+dropArea.addEventListener('dragleave', () => {
+  dropArea.classList.remove('dragglow');
+});
+
+
+// ill continue w/ this later 
